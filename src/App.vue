922d228610d7db5eab1ca602e37da4077791b3d3
madewithbill <165756396+madewithbill@users.noreply.github.com>
2026-07-24 14:55:30 -0700

    feat: add initial layout and styling

--- src/App.vue
+++ src/App.vue
@@ -33,60 +33,80 @@ watch(currentType, () => {
 </script>
 
 <template>
+  <header class="uppercase px-4 h-12 font-semibold flex items-center border-b border-b-neutral-300">
+    Bike finder
+  </header>
   <main>
-    <section class="min-h-dvh w-full flex flex-col justify-center items-center">
-      <div>
-        <h1 class="text-5xl mb-2">bike finder</h1>
+    <section class="w-full grid grid-cols-2 items-start">
+      <div class="bg-neutral-100 min-h-dvh">
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/AziEScuczQYWXQPNMDteB5.jpg"
+          alt=""
+          class="w-full h-120 object-cover"
+        />
         <p>Recommened size={{ currentSize.alphaSize }}</p>
         <p v-if="currentType === 'Road'">Size in CM={{ currentSize.cmSize }}</p>
-        <div class="flex gap-4">
-          <button
-            v-for="bike in bikeTypes"
-            @click="selectType(bike)"
-            class="px-4 py-2 rounded-sm hover:cursor-pointer transition-colors duration-300 font-medium"
-            :class="currentType === bike ? 'bg-amber-200 ' : 'bg-neutral-200 hover:bg-neutral-300'"
-          >
-            {{ bike }}
-          </button>
-        </div>
-        <p>Selected bike type: {{ currentType }}</p>
       </div>
-      <div>
-        <label for="feet">feet</label>
-        <input
-          @input="setSizeByHeight"
-          v-model.number="currentFeet"
-          id="feet"
-          type="number"
-          min="4"
-          max="6"
-          :disabled="currentType === 'Road'"
-          class="h-8 border"
-        />
-        <label for="inches">inches</label>
-        <input
-          @input="setSizeByHeight"
-          v-model.number="currentInches"
-          id="inches"
-          type="number"
-          min="0"
-          max="11"
-          :disabled="currentType === 'Road'"
-          class="h-8 border"
-        />
-        <p>current height is: {{ currentHeight }}</p>
-        <label for="inseam">inches</label>
-        <input
-          @input="setRoadSize"
-          v-model.number="currentInseam"
-          id="inseam"
-          type="number"
-          min="26"
-          max="39"
-          :disabled="currentType !== 'Road'"
-          class="h-8 border"
-        />
-        <p>current inseam is: {{ currentInseam }}</p>
+      <div class="px-8 py-8 w-full max-w-120 justify-self-center grid grid-cols-1 gap-4">
+        <h1 class="text-4xl font-semibold">Find your bike size</h1>
+        <div>
+          <span class="font-medium">Select bike type</span>
+          <div class="grid grid-cols-3 gap-1">
+            <button
+              v-for="bike in bikeTypes"
+              @click="selectType(bike)"
+              class="px-4 py-2 rounded-sm hover:cursor-pointer transition-colors duration-300 font-medium"
+              :class="
+                currentType === bike ? 'bg-amber-200 ' : 'bg-neutral-200 hover:bg-neutral-300'
+              "
+            >
+              {{ bike }}
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <span class="font-medium">Measurements</span>
+          <div class="grid grid-cols-2 gap-1">
+            <label
+              >Feet
+              <input
+                @input="setSizeByHeight"
+                v-model.number="currentFeet"
+                name="feet"
+                type="number"
+                min="4"
+                max="6"
+                :disabled="currentType === 'Road'"
+              />
+            </label>
+            <label
+              >Inches
+              <input
+                @input="setSizeByHeight"
+                v-model.number="currentInches"
+                name="inches"
+                type="number"
+                min="0"
+                max="11"
+                :disabled="currentType === 'Road'"
+              />
+            </label>
+          </div>
+        </div>
+
+        <label
+          >Inseam
+          <input
+            @input="setRoadSize"
+            v-model.number="currentInseam"
+            name="inseam"
+            type="number"
+            min="26"
+            max="39"
+            :disabled="currentType !== 'Road'"
+          />
+        </label>
       </div>
     </section>
   </main>
