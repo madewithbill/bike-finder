5d606b1f25cc58b9a34bca0c07b82591d3409b3f
madewithbill <165756396+madewithbill@users.noreply.github.com>
2026-08-30 21:26:59 -0700

    refactor: create reusable price formatter

---
+++ src/utils/formatPrice.ts
@@ -0,0 +1,7 @@
+export function formatPrice(price: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+  }).format(price / 100)
+}
