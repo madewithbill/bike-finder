416c94c68683161f931a0ae6e74a9bbe9b1b50a9
madewithbill <165756396+madewithbill@users.noreply.github.com>
2026-09-18 22:57:35 -0700

    feat: add supabase local development setup

---
+++ supabase/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO "public"."bikes"
+    ("id", "name", "bike_type", "price", "in_stock", "on_sale", "sale_price", "main_image")
+VALUES 
+    (1, 'Domane AL 5 Gen 4', 'Road', 199999, true, false, null, 'DomaneAL5-27-80404-C-Primary.avif'), 
+    (2, 'Slash 9.9 X0 AXS T-Type Gen 6', 'MTB', 869999, true, true, 779993, 'Slash99-26-52939-B-Primary.avif'), 
+    (3, 'Madone SLR 9 AXS Gen 8', 'Road', 1349999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/MadoneSLR9AXS-27-46151-I-Primary.avif'), 
+    (6, 'FX Sport AL 3', 'City', 99999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/FX3Sport-26-58592-A-Primary.avif'), 
+    (7, 'Speed Concept SLR 7', 'Road', 949999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/SpeedConceptSLR9AXS-25-48020-F-Primary.avif'), 
+    (8, 'Checkpoint SL 6 AXS Gen 3', 'Road', 369999, false, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/CheckpointSL6AXS-26-55098-A-Primary.avif'), 
+    (10, 'Fuel+ LX 9.9 X0 AXS Gen 2', 'MTB', 1019999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/FuelPlusLX99X0AXS-27-81737-B-Primary.avif'), 
+    (18, 'Domane SLR 9 AXS No. 76 Gen 5', 'Road', 1449999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/DomaneSLR9AXSNo76-27-81730-A-Primary.avif'), 
+    (19, 'Rail+ 8 Gen 5', 'MTB', 599999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/RailPlus8-27-79218-A-Primary.avif'), 
+    (20, 'FX 2 Midstep', 'City', 79999, true, false, null, 'https://qyrqgkokuudocsayolmf.supabase.co/storage/v1/object/public/bikes/FX2Midstep-26-57142-A-Primary.avif');
